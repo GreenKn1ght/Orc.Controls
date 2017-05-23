8d10baaebe3e07e210e7937d661945e63126e590
michu <michalp@op.pl>
2017-05-23 18:58:58 +0200

    Rename DateRangePicker properties

--- src/Orc.Controls/Orc.Controls.Shared/Controls/DateRangePicker/Views/DateRangePicker.xaml.cs
+++ src/Orc.Controls/Orc.Controls.Shared/Controls/DateRangePicker/Views/DateRangePicker.xaml.cs
@@ -94,31 +94,31 @@ namespace Orc.Controls
         public static readonly DependencyProperty AllowCopyPasteProperty = DependencyProperty.Register("AllowCopyPaste", typeof(bool),
             typeof(DateRangePicker), new FrameworkPropertyMetadata(true));
 
-        public string DateTimeFormat
+        public string Format
         {
-            get { return (string)GetValue(DateTimeFormatProperty); }
-            set { SetValue(DateTimeFormatProperty, value); }
+            get { return (string)GetValue(FormatProperty); }
+            set { SetValue(FormatProperty, value); }
         }
 
-        public static readonly DependencyProperty DateTimeFormatProperty = DependencyProperty.Register("DateTimeFormat", typeof(string),
+        public static readonly DependencyProperty FormatProperty = DependencyProperty.Register("Format", typeof(string),
             typeof(DateRangePicker), new FrameworkPropertyMetadata(CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern + " " + CultureInfo.CurrentCulture.DateTimeFormat.LongTimePattern));
 
-        public bool DateTimeHideTime
+        public bool HideTime
         {
-            get { return (bool)GetValue(DateTimeHideTimeProperty); }
-            set { SetValue(DateTimeHideTimeProperty, value); }
+            get { return (bool)GetValue(HideTimeProperty); }
+            set { SetValue(HideTimeProperty, value); }
         }
 
-        public static readonly DependencyProperty DateTimeHideTimeProperty = DependencyProperty.Register("DateTimeHideTime", typeof(bool),
+        public static readonly DependencyProperty HideTimeProperty = DependencyProperty.Register("HideTime", typeof(bool),
             typeof(DateRangePicker), new FrameworkPropertyMetadata(false));
 
-        public bool DateTimeHideSeconds
+        public bool HideSeconds
         {
-            get { return (bool)GetValue(DateTimeHideSecondsProperty); }
-            set { SetValue(DateTimeHideSecondsProperty, value); }
+            get { return (bool)GetValue(HideSecondsProperty); }
+            set { SetValue(HideSecondsProperty, value); }
         }
 
-        public static readonly DependencyProperty DateTimeHideSecondsProperty = DependencyProperty.Register("DateTimeHideSeconds", typeof(bool),
+        public static readonly DependencyProperty HideSecondsProperty = DependencyProperty.Register("HideSeconds", typeof(bool),
             typeof(DateRangePicker), new FrameworkPropertyMetadata(false));
 
         public bool ShowOptionsButton
